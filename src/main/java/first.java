885b8a6e38676a4bcb359baccf31069ee6957903
wpp <307208969@qq.com>
2022-12-04 22:50:19 +0800

    hot-fix commmit1

--- src/main/java/first.java
+++ src/main/java/first.java
@@ -3,5 +3,6 @@ public class first {
         System.out.println("第一个idea上传的github项目1");
         System.out.println("second commit");
         System.out.println("third commit");
+        System.out.println("hot-fix commit1");
     }
 }
