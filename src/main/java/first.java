6ef2f504d9db786e2f9147e83eb6a2003ffcc601
wpp <307208969@qq.com>
2022-12-04 22:29:27 +0800

    third commit

--- src/main/java/first.java
+++ src/main/java/first.java
@@ -2,5 +2,6 @@ public class first {
     public static void main(String[] args) {
         System.out.println("第一个idea上传的github项目1");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
